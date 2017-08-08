66d8af62d11a584a4ab67f741f4dfde7db7eb877
Max Naumov <maxnaumovs@gmail.com>
2017-08-09 00:45:27 +0300

    reconfigured web api to use owin server removed Global.asax.cs file since web api will use owin server now added context class added the connection string added user model added authentification repository added account controller added registration form added angularjs module with route configuration, auth service, registration controller.

--- PersonalTaskManagerWeb/Controllers/AccountController.cs
+++ PersonalTaskManagerWeb/Controllers/AccountController.cs
@@ -109,41 +109,5 @@ namespace PersonalTaskManagerWeb.Controllers
 
             base.Dispose(disposing);
         }
-
-        private class LoginData
-        {
-            public string LoginProvider { get; set; }
-            public string ProviderKey { get; set; }
-            public string UserName { get; set; }
-            public string ExternalAccessToken { get; set; }
-
-            public static LoginData FromIdentity(ClaimsIdentity identity)
-            {
-                if (identity == null)
-                {
-                    return null;
-                }
-
-                Claim providerKeyClaim = identity.FindFirst(ClaimTypes.NameIdentifier);
-
-                if (providerKeyClaim == null || String.IsNullOrEmpty(providerKeyClaim.Issuer) || String.IsNullOrEmpty(providerKeyClaim.Value))
-                {
-                    return null;
-                }
-
-                if (providerKeyClaim.Issuer == ClaimsIdentity.DefaultIssuer)
-                {
-                    return null;
-                }
-
-                return new LoginData
-                {
-                    LoginProvider = providerKeyClaim.Issuer,
-                    ProviderKey = providerKeyClaim.Value,
-                    UserName = identity.FindFirstValue(ClaimTypes.Name),
-                    ExternalAccessToken = identity.FindFirstValue("ExternalAccessToken"),
-                };
-            }
-        }
     }
 }
